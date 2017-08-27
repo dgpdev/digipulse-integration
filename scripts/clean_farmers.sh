1cdbf41dc83844e5796695c28101f114cca19e8e
Tarmi <developer@clustertoken.io>
2017-08-27 02:41:21 +0200

    major

--- scripts/clean_farmers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in `seq 1 16`; do
-    rm -rfv /root/config/storj-share/farmer-$i/contracts.db
-    rm -rfv /root/config/storj-share/farmer-$i/sharddata.kfs
-done
--- scripts/stop_everything.sh
+++
@@ -1,4 +0,0 @@
-service redis-server stop
-service rabbitmq-server stop
-service mongod stop
-pm2 delete all
